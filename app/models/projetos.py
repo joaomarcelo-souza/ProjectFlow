f26dd52a5fe7998dc84415ae3da842f9647c1c83
João Souza <marceloamaral201924@gmail.com>
2026-01-07 06:54:14 -0400

    feat: ORM table projetos created

---
+++ app/models/projetos.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from app.db.database_config import Base
+
+
+class Projeto(Base):
+
+    __tablename__ = "projetos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(nullable=False)
+    descricao: Mapped[str] = mapped_column(nullable=False)
+
+    tarefas: Mapped[list["Tarefa"]] = relationship(
+        back_populates="projeto", cascade="all, delete_orphan"
+    )
+
+
+from app.models.tarefas import Tarefa

4ed746be0e86b2fd0e9139a62880e58aeab6e01d
João Souza <marceloamaral201924@gmail.com>
2026-01-09 09:05:35 -0400

    feat: projetos_routes added to the project

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.db.database_config import Base, engine
+from app.api.v1.projetos_routes import router as projetos_router
 
 app = FastAPI(title="Project Flow")
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(router=projetos_router)
